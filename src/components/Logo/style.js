e0cdf73c17766a5452759e6e3cad7aa2221af178
FabricioMF72 <55679079+FabricioMF72@users.noreply.github.com>
2020-09-08 13:13:26 -0600

    config vercel

---
+++ src/components/Logo/style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Svg = styled.svg`
+  width: 100%; 
+  position: relative;
+  display: block;
+  overflow: hidden;
+  margin: 20px 100px 20px 0;
+`
